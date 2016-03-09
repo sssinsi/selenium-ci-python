900fd9b7d20e36b42e70ec31410e4e4cc7fb620a
sssinsi <tet643773@gmail.com>
2016-03-09 23:14:21 +0900

    remove not use import and deprecated function.
    不要なコードを消しました。

--- scripts/webdriver_sample.py
+++ scripts/webdriver_sample.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
+
 
 class WebdriverSample(unittest.TestCase):
     def setUp(self):
@@ -14,36 +12,42 @@ class WebdriverSample(unittest.TestCase):
         self.base_url = "https://google.com"
         self.verificationErrors = []
         self.accept_next_alert = True
-    
+
     def test_webdriver_sample(self):
         driver = self.driver
         driver.get(self.base_url + "/")
         self.assertEqual("Google", driver.title)
-    
+
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
         return True
-    
+
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException as e: return False
+        try:
+            self.driver.switch_to.alert()
+        except NoAlertPresentException as e:
+            return False
         return True
-    
+
     def close_alert_and_get_its_text(self):
         try:
-            alert = self.driver.switch_to_alert()
+            alert = self.driver.switch_to.alert()
             alert_text = alert.text
             if self.accept_next_alert:
                 alert.accept()
             else:
                 alert.dismiss()
             return alert_text
-        finally: self.accept_next_alert = True
-    
+        finally:
+            self.accept_next_alert = True
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
+
 
 if __name__ == "__main__":
     unittest.main()
